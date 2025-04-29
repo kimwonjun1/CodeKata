fd256d68657f6cf6b22f2791a9c543b6a9f34cf7
kimwonjun1 <monkeydmw@naver.com>
2025-04-29 09:09:34 +0900

    Multiply class modified

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Subtraction subtraction = new Subtraction();
-        int answer = subtraction.solution(3, 1);
-        System.out.println("answer = " + answer);
+        int subAnswer = subtraction.solution(3, 1);
+        System.out.println("answer = " + subAnswer);
+
+        Multiply multiply = new Multiply();
+        int mulAnswer = multiply.solution(3, 3);
+        System.out.println("mulAnswer = " + mulAnswer);
     }
 }
