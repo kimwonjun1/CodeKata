bf1e240cef556f03137016ab9cd0a8a1fb159bd8
kimwonjun1 <monkeydmw@naver.com>
2025-05-08 09:16:40 +0900

    EvenOdd class add

---
+++ src/EvenOdd.java
@@ -0,0 +1,13 @@
+public class EvenOdd {
+    public String solution(int num) {
+        String answer = "";
+        if(num % 2 == 0) {
+            answer = "Even";
+        } else if ((num % 2) != 0) {
+            answer = "Odd";
+        } else {
+            System.out.println("정수를 입력해주세요.");
+        }
+        return answer;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -40,5 +40,9 @@ public class Main {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         double arrAvg = arrAverage.solution(numbers);
         System.out.println("arrAvg = " + arrAvg);
+
+        EvenOdd evenOdd = new EvenOdd();
+        String answer = evenOdd.solution(5);
+        System.out.println("answer = " + answer);
     }
 }
